ed82c196a8afd83b65e4a70dd06790d740726b15
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-09-30 13:16:26 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -2,7 +2,7 @@ public class Greet {
 	public static void main(String [] args) {
 		greet(String);
 	}
-	public static void greet(String){
+	public static void greet(String [] args){
 		System.out.print("Hello World");
 	}
 }
