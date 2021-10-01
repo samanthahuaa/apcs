f5b341788b487d371af0daeae0d8b060a80eb1a6
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-09-30 23:15:45 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,11 @@
+// Samantha Hua
+// APCS
+// HW08 -- Methods Calling Methods
+// 2021-09-30
+
+// Note: Angle brackets (<>) denote a field. Replace them with the indicated info.
+// (e.g., Replace "<mm>" with "09" for September).
+
 //DISCOVERIES
 // I referred back to my summer pre-work exercises to remind myself of how to call functions and pass information into them.
 // In particular, I was reminded of how when a method has a parameter, you must specify the type of information that is being passed into it.
