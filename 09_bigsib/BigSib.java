5272c9acf07038dbea3ee222d6b1088a9b6a9172
Samantha Hua <shua30@stuy.edu>
2021-10-01 13:16:50 -0400

    homework 09

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,11 @@
+public class Greet {
+	public static void main(String[] args) {
+		BigSib.greet("Flim");
+		BigSib.greet("Flam");
+		BigSib.greet("Kazaam");
+	}
+
+	public static void BigSib(String name) {
+		System.out.println("Hello name! I'm Poppy (not poopy)")
+	}
+}
