7ca35a6f02c5f3d4fcfba4edf45ddae53f734b0c
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-09-30 23:11:31 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,10 @@
+//DISCOVERIES
+// I referred back to my summer pre-work exercises to remind myself of how to call functions and pass information into them.
+// In particular, I was reminded of how when a method has a parameter, you must specify the type of information that is being passed into it.
+
+//UNRESOLVED QUESTIONS
+// At the moment, I do not have any unresolved questions.
+
 public class Greet {
 	public static void main(String [] args) {
 		greet("Maria");
