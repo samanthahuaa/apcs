6e6ca1cc70ab90c71bfc40139ac71e49cc61cfca
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-02 15:00:52 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -4,11 +4,13 @@
 // 2021-10-02
 
 // Discoveries
-// You can call other methods from other files without needing to import anything. The method that you are calling should not have main(String[] args).
-// To call a method in another file, write [FileName].[MethodName]([AnyParameters)
+// You can call other methods from other files without needing to import anything. The method that you are calling can also have a main().
+// To call a method in another file, write [FileName].[MethodName]([AnyParameters). 
 
 //Unresolved Questions
-// Why can't the file being called have a main(String[] args) method?
+// Why do we normally write main(String[] args)? Why not something else?
+
+// Check the BigSib.java file to see my process of experimentation.
 
 public class Greet {
 	public static void main(String[] args) {
@@ -16,4 +18,9 @@ public class Greet {
 		BigSib.main("Flam");
 		BigSib.main("Kazaam");
 	}
+// 	public static void main(String[] args) {
+// 		BigSib.greet("Flim");
+// 		BigSib.greet("Flam");
+// 		BigSib.greet("Kazaam");
+// 	}
 }
