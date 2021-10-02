09888a62ee73248459d6f8e4bde36095c06f3859
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-02 13:40:44 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,5 +1,5 @@
 public class BigSib {
-	public static void main(String[] args) {
-		System.out.println("Hello "+args+"! I'm Poppy (not poopy)");
+	public static void greet(String name) {
+		System.out.println("Hello "+name+"! I'm Poppy (not poopy)");
 	}
 }
