0cc339e772962b4fb6799bb95d5cbae3cdd6f35e
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-02 14:15:09 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -3,6 +3,13 @@
 // HW09 -- Calling methods from other java files
 // 2021-10-02
 
+// Discoveries
+// You can call other methods from other files without needing to import anything. The method that you are calling should not have main(String[] args).
+// To call a method in another file, write [FileName].[MethodName]([AnyParameters)
+
+//Unresolved Questions
+// Why can't the file being called have a main(String[] args) method?
+
 public class Greet {
 	public static void main(String[] args) {
 		BigSib.greet("Flim");
