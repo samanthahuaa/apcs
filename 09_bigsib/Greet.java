5b2f8ab53362db3765306fbeb822e465b5e555c6
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-02 14:15:21 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -12,8 +12,8 @@
 
 public class Greet {
 	public static void main(String[] args) {
-		BigSib.greet("Flim");
-		BigSib.greet("Flam");
-		BigSib.greet("Kazaam");
+		BigSib("Flim");
+		BigSib("Flam");
+		BigSib("Kazaam");
 	}
 }
