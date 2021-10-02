a07cd63abab29756e4bd059a1b8659a354776678
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-02 14:58:54 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,4 +1,8 @@
 // For the heading, look at Greet.java
+// I first tried to use a method called greet because I faced errors when using main. 
+// I then revisted using main as a method and found that I needed to write BigSib.main in order for Greet.java to look at BigSib as a file.
+// I also needed to write BigSib.main similar to how I wrote BigSib.greet.
+// The next error I encountered was that the parameters for main took in a array of strings so I edited that to only take in one string. It worked!
 
 public class BigSib {
 	public static void main(String args) {
