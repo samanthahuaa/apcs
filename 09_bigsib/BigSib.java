a9c8a65119c5eca8ca718bf2711f9c390bdc9c6d
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-02 14:18:09 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,7 +1,7 @@
 // For the heading, look at Greet.java
 
 public class BigSib {
-	public static void main(String[] args) {
+	public static void main(String args) {
 		System.out.println("Hello "+args+"! I'm Poppy (not poopy)");
 	}
 // 	public static void greet(String name) {
