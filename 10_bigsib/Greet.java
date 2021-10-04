ea9229f6fa0b968fbf124789165ba0be7524f2e7
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-04 18:17:05 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,3 +1,11 @@
+/*
+Samantha Hua
+APCS
+HW10 -- Methods that return values that can be used by other methods
+2021-10-4
+
+*/
+
 public class Greet {
     public static void main( String[] args ){
         System.out.println( BigSib.greet("Crosby") );
