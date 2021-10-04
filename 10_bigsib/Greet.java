e0ca16d191f45aeb0f0413d231f5fedeb969e517
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-04 18:34:30 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -4,6 +4,12 @@ APCS
 HW10 -- Methods that return values that can be used by other methods
 2021-10-4
 */
+/*
+DISCOVERIES
+You can call other methods inside a System.out.println statement. (It won't just print out the name of the method)
+UNRESOLVED QUESTIONS
+No questions as of now.
+*/
 
 public class Greet {
     public static void main( String[] args ){
