950f4ab8c62ec6ab0b63f3c4e1f4eb135a7b18a6
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-05 22:19:45 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -8,7 +8,7 @@ public class Greet {
     String greeting;
 
     BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+//     richard.setHelloMsg("Word up");
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
