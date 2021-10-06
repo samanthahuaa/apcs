107b43abf24b7809616f65ea2f597296907508fc
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-06 18:19:39 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -3,7 +3,7 @@ public class Greet {
     String greeting;
 
     BigSib richard = new BigSib();
-    richard.setHelloMsg("Word up");
+//     richard.setHelloMsg("Word up");
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
