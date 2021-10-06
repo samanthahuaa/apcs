cab2e7b55fe2c97f787745a1746d8c0193150efc
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-05 23:04:55 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,9 +1,9 @@
 public class BigSib {
   public static void setHelloMsg(String a) {
-    System.out.print(a);
+    System.out.print(a+" ");
   }
   
-  public static String greet( String a+" ") {
+  public static String greet( String a) {
     return a+"\nSalutations Dr. Spaceman \nHey ya Kong Fooey \nSup mom";
   }
 }
