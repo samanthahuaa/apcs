26b7a8124e4a1db4d8a1ab1586b30fe62caa9821
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-05 22:12:03 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,3 +1,8 @@
+/*
+Flaming Hot Cheetos (Perry (Salaj Jr.), Courtney (Timothy), Samantha (me) (Poppy)
+
+*/
+
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
