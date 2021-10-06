196d5901f5e2580e00f6ce4a4f59391c0c34c943
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-06 17:20:05 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,11 +1,11 @@
 public class BigSib {
   String helloMsg;
   
-  public static void setHelloMsg(String a) {
+  public void setHelloMsg(String a) {
     helloMsg = a;
   }
   
-  public static String greet( String a) {
+  public String greet( String a) {
     return helloMsg+"\nSalutations Dr. Spaceman \nHey ya Kong Fooey \nSup mom";
   }
 }
