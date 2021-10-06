9dbae258ca49dea224d2c9a036eb414ffa788ba2
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-05 23:28:21 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,6 +1,15 @@
 /*
 Flaming Hot Cheetos (Perry (Salaj Jr.), Courtney (Timothy), Samantha (me) (Poppy)
-
+APCS
+HW12 -- Passing methods into other classes + Utilizing return statements in methods + new function
+2021-10-05
+*/
+/*
+Discoveries
+\n will create a new line in the output even if it's in quotations
+the new function doesn't seem to create another file but it runs the same way as the funcion it calls
+QCC
+What is the new function good for? In what situations would it be better to use new rather than the actual file itself?
 */
 
 public class Greet {
