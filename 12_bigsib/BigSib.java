e29b786615cf968c9d6d91ccb141d86cad73cd90
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-05 22:21:13 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,5 +1,5 @@
 public class BigSib {
   public static String greet( String a) {
-    return a+"Salutations Dr. Spaceman \n Hey ya Kong Fooey \n Sup mom"
+    return a+"Salutations Dr. Spaceman \n Hey ya Kong Fooey \n Sup mom";
   }
 }
