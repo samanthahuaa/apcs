69cee00c3b5233835261d32d2d3eb941641f4bf3
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-06 20:25:12 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,3 +1,16 @@
+/*
+Frogs on Fire: Andrew Piatetsky and Samantha Hua
+APCS
+HW13 -- Using a Constructor to Simplify the Process
+2021-10-06
+*/
+/*
+Discoveries:
+Constructors must be in the class that it is for.
+You can pass arguments into constructors
+QCC:
+What's a real life example of the usage of a constructor?
+*/
 public class Greet {
   
   public static void main( String[] args ) {
