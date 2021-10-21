13ecd248cab95a7744187eecab4c6f96fac00db1
Samantha Hua <shua30@stuy.edu>
2021-10-20 21:43:26 -0400

    test

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+	public static void main(String[] args) {
+		double d = 4.0;
+		System.out.println(d.getClass().getSimpleName());
+		long l = Math.round(d);
+		System.out.println(l.getClass().getSimpleName());
+		l.getClass().getSimpleName();
+		i = Math.round(l);
+		System.out.println(i.getClass().getSimpleName());
+	}
+}
