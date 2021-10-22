636c445fd1a50cc856a571c0512eaf34819b2b28
Samantha Hua <shua30@stuy.edu>
2021-10-22 00:10:18 -0400

    testing

---
+++ test/division.java
@@ -0,0 +1,13 @@
+public class division {
+	public static void main(String[] args) {
+	System.out.println(5/3);
+	System.out.println(5.0/3);
+	System.out.println(5/3.0);
+	System.out.println(-5/3);
+	System.out.println(5/-3);
+	System.out.println(-5/-3);
+	System.out.println(-5.0/3);
+	System.out.println(-5/3.0);
+	System.out.println(-5.0/-3.0);
+}
+}
