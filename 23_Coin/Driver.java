bc8f5ed01c8319c24dec5820af800e3602f0f22b
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-25 22:21:13 -0400

    Update Driver.java

--- 23_Coin/Driver.java
+++ 23_Coin/Driver.java
@@ -4,7 +4,6 @@ APCS
 HW23 -- What Does Equality Look Like?
 2021-10-23
 time spent: 0.7 hour
-
 DISCO
 - We should read ahead before we start coding in order to minimize confusion. 
 (For the toString() method we were trying to complete tasks that we are not capable of doing given the things we have learned in class.)
@@ -12,7 +11,6 @@ DISCO
 - We realized we needed to set upFace to heads in all of the constructors in order for our code to behave the way we wanted it to.
 - If bias does not have a value set in the constructor, its value will automatically be 0 meaning that the probability of the coin flipping to heads would be 0 regardless of what Math.random() is.
 - If Math.random()*bias is not multiplied by two, the probability of the coin flipping to heads would still be 0. This is because the max value for Math.random() is 0.999 so when multiplied by 0.5(the value we set for bias), probability will never reach 0.5.
-
 QCC
 - Is it possible to call upon an object's name?
 	ex: ClassName ObjectName = new ClassName(); a function that would return ObjectName
@@ -20,37 +18,27 @@ QCC
 - Does "Coin other" as an input refer to another object? How do we know which Object it is referring to? How can it reference an instance of a Class inside an instance of the same Class?
 */
 
-public class Driver { 
+/***
+    driver for class Coin
+    ~~~ SUGGESTED WORKFLOW: ~~~
+    1. Compile this file and run. Note anything notable.
+    2. Move the "TOP" line down, so that it is below the first statement.
+    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
+    (your editor: ???)
+    3. Compile and run again.
+    4. Resolve errors one at a time until it works.
+    5. Repeat 2-4 until TOP meets BOTTOM.
+***/
+
+public class Driver {
+
   public static void main( String[] args ) {
-	  Coin bob = new Coin();
-	Coin natasha = new Coin();
-	  int x = 100;
-	  int y = 120;
-	  int totalHeads = 0;
-	  int matchCounter = 0;
-	  while(totalHeads < x || matchCounter < y || matchCounter < 65536 || matchCounter%2005 != 0){
-		  if(bob.flip() == "heads"){
-			  totalHeads += 1;
-		  }
-		  if(natasha.flip() == "heads"){
-			  totalHeads += 1;
-		  }
-		  if(bob.equals(natasha)){
-			  matchCounter += 1;
-		  }
-	  }
-	  int totalFlips = bob.flipCtr + natasha.flipCtr;
-	  System.out.println("Success!");
-	  System.out.println("The total number of flips was: " + totalFlips);
-	  System.out.println("The total amount of heads was: " + totalHeads);
-	  System.out.println("The total number of matches was: " + matchCounter);
-	  
-	  
-	  
+
     //build Objects from blueprint specified by class Coin
 
+
     //test default constructor
-    /*
+    
       Coin mine = new Coin();
       //test 1st overloaded constructor
       Coin yours = new Coin( "quarter" );
@@ -77,7 +65,8 @@ public class Driver {
       else {
         System.out.println( "No match. Firestarter you can not be." );
       }
-	  */
+	  /*===================TOP==========================
+      ====================BOTTOM======================*/
 
   }//end main()
 
