73678c0c1e3ed9cbd826ff2486c24bcd78b4db26
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-27 19:59:13 -0400

    Update Stats.java

---
+++ 26_gcd/Stats.java
@@ -0,0 +1,26 @@
+public class Stats {
+  public static int gcd( int a, int b ){
+    int gcd = 1;
+    int counter = 1;
+    if (a < b) {
+      while (counter < a) {
+        if (a%counter == 0 && b%counter == 0) {
+          gcd = counter;
+        }
+        counter++;
+      }
+    } else {
+      while (counter < b) {
+          if (a%counter == 0 && b%counter == 0) {
+            gcd = counter;
+          }
+          counter++;
+        }
+    }
+  }
+  
+  public static void main(String[] args) {
+    gcd(9,15);
+    gcd(3,14);
+  }
+}
