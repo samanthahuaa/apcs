86aa23be113decb6237a3ab7d0d5cd4e082a1144
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-26 22:24:34 -0400

    Update Repeater.java

---
+++ 25_repeat/Repeater.java
@@ -0,0 +1,14 @@
+public class Repeater {
+  public static void main(String[] args){
+  }
+  
+  public String fenceW(int numPosts) {
+    int counter = 0;
+    String fence;
+    if (numPosts == 1) {
+      return 
+    while (counter < numPosts) {
+      
+    }
+  }
+}
