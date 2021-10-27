fd193134a491495e821259dd720ece56a8ea4949
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-26 22:41:17 -0400

    Update Repeater.java

--- 25_repeat/Repeater.java
+++ 25_repeat/Repeater.java
@@ -1,6 +1,6 @@
 public class Repeater {
   public static void main(String[] args){
-    System.out.println(fenceW(1);)
+    System.out.println(fenceW(1));
 //     System.out.println();
     System.out.println(fenceW(2));
     System.out.println(fenceW(5));
