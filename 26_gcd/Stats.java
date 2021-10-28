27213f6bfd61692b40d06d4cd8e4a59247867959
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-27 20:04:25 -0400

    Update Stats.java

--- 26_gcd/Stats.java
+++ 26_gcd/Stats.java
@@ -20,8 +20,23 @@ public class Stats {
     return gcd;
   }
   
+  public static int gcdER( int a, int b ) {
+    if (a == b) {
+      return a;
+    } else {
+      if (a > b) {
+        return gcd(a-b,b);
+      } else {
+        return gcd(b-a,a);
+      }
+    }
+  }
+  
   public static void main(String[] args) {
     System.out.println(gcd(9,15));
     System.out.println(gcd(3,14));
+    
+    System.out.println(gcdER(9,15));
+    System.out.println(gcdER(3,14));
   }
 }
