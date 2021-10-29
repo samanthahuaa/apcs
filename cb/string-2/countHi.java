eb9ad25e3bf0e9bdfce6c1c0bd7e74cace851497
Samantha Hua <shua30@stuy.edu>
2021-10-29 13:18:40 -0400

    countHi edits

---
+++ cb/string-2/countHi.java
@@ -0,0 +1,19 @@
+public class countHi {
+
+	public static  int countHi(String str) {
+	  int counter = 0;
+	  for (int i=0; i < str.length(); i++) {
+	    if (str.substring(i,i+1) == "h") {
+	      if (str.substring(i+1,i+2) == "i") {
+		System.out.println("ran");
+	        counter++;
+	      }
+	    }
+	  }
+	  return counter;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(countHi("hihihi"));
+	}
+}
