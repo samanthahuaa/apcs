ba810f1a688819799b79fbdf16c66bb5fa4a22ea
Samantha Hua <shua30@stuy.edu>
2021-10-29 13:22:29 -0400

    updates

--- cb/string-2/countHi.java
+++ cb/string-2/countHi.java
@@ -3,7 +3,9 @@ public class countHi {
 	public static  int countHi(String str) {
 	  int counter = 0;
 	  for (int i=0; i < str.length(); i++) {
+		System.out.println("ran outside");
 	    if (str.substring(i,i+1) == "h") {
+			System.out.println("ran once");
 	      if (str.substring(i+1,i+2) == "i") {
 		System.out.println("ran");
 	        counter++;
