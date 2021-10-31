d1d19796d9e47d5cc2f7435817852cbf84880169
samanthahuaa <samanthahua1@gmail.com>
2021-10-30 22:20:53 -0400

    homework

--- cb/string-2/catDog.java
+++ cb/string-2/catDog.java
@@ -2,8 +2,25 @@
 
 public class catDog {
   public static boolean catDog(String str) {
+    int catCounter = 0;
+    int dogCounter = 0;
     for (int i=0; i < (str.length()-2); i++) {
-      
+      if (str.substring(i,i+3).equals("cat")) {
+        catCounter++;
+      } else if (str.substring(i,i+3).equals("dog")) {
+        dogCounter++;
+      }
     }
+    if (catCounter == dogCounter) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(catDog("catdog"));
+    System.out.println(catDog("catcat"));
+    System.out.println(catDog("1cat1cadodog"));
   }
 }
