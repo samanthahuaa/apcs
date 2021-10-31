6bd6f15e5d86105edd24b2eb217cbed037f728b6
samanthahuaa <samanthahua1@gmail.com>
2021-10-30 22:40:25 -0400

    homework

---
+++ cb/string-2/endOther.java
@@ -0,0 +1,35 @@
+//Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+
+public class endOther {
+  public static boolean endOther(String a, String b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    if (a.length() == b.length()) {
+      if (a.equals(b)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (a.length() > b.length()) {
+      String substring = a.substring(a.length() - b.length(),a.length());
+      if (substring.equals(b)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      String substring = b.substring(b.length() - a.length(),b.length());
+      if (substring.equals(a)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(endOther("Hiabc", "abc"));
+    System.out.println(endOther("AbC", "HiaBc"));
+    System.out.println(endOther("abc", "abXabc"));
+  }
+}
