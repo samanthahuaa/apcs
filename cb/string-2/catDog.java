2d984931e177dad7ae9194ee4de3610de3c6d9ae
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-30 22:14:58 -0400

    Update catDog.java

--- cb/string-2/catDog.java
+++ cb/string-2/catDog.java
@@ -1,5 +1,7 @@
-Return true if the string "cat" and "dog" appear the same number of times in the given string.
+// Return true if the string "cat" and "dog" appear the same number of times in the given string.
 
 public class catDog {
-  
+  public static boolean catDog(String str) {
+    
+  }
 }
