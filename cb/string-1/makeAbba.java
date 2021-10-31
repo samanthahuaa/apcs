2b182c631833ffc01dba4dc09b91ff8525064e8e
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-10-30 21:30:15 -0400

    Update makeAbba.java

---
+++ cb/string-1/makeAbba.java
@@ -0,0 +1,11 @@
+public class makeAbba {
+  public static String makeAbba(String a, String b) {
+    return a+b+b+a;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(makeAbba("Hi", "Bye"));
+    System.out.println(makeAbba("x", ""));
+    System.out.println(makeAbba("", "y"));
+  }
+}
