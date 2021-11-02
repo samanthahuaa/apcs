be9ef677562f1c190c1338aa21a2c97c0e37f1f6
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-11-02 19:12:59 -0400

    Update Commafier.java

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -1,3 +1,16 @@
+/*
+QUAD PUFFS: Yat Long Chan, Lindsay Phung, David Deng, Samantha Hua
+APCS
+HW29 -- Which Way Do You Roll? // commafy an int iteratively and recursively
+2021-11-01
+time spent: 0.8 hrs
+DISCO:
+- Wrapper classes have a variety of methods including converting between variable types.
+- FOREACH loops go through every single element in an array, similar to Python's FOR loop.
+QCC:
+- Recursive methods are sometimes very unintuitive. e.g. commafyR() 
+*/
+
 public class Commafier{
 
   public static String commafyF(int n){
@@ -26,19 +39,19 @@ public class Commafier{
     return commafyR(newNum) + "," + strInt.substring(strInt.length() - 3);
   }
   public static void main (String[] args){
-    System.out.println(commafyF(1));
+    /* System.out.println(commafyF(1));
     System.out.println(commafyF(101));
     System.out.println(commafyF(1000));
     System.out.println(commafyF(12345));
     System.out.println(commafyF(4598765));
     System.out.println(commafyF(100000000));
-//     System.out.println("---RECURSIVE TESTING---");
-//     System.out.println(commafyR(1));
-//     System.out.println(commafyR(101));
-//     System.out.println(commafyR(1000));
-//     System.out.println(commafyR(12345));
-//     System.out.println(commafyR(4598765));
-//     System.out.println(commafyR(100000000));
+    System.out.println("---RECURSIVE TESTING---");
+    System.out.println(commafyR(1));
+    System.out.println(commafyR(101));
+    System.out.println(commafyR(1000));
+    System.out.println(commafyR(12345));
+    System.out.println(commafyR(4598765));
+    System.out.println(commafyR(100000000)); */
     for(String s : args){
       System.out.println("RECURSIVE TEST: " + commafyR(Integer.parseInt(s)) + "\n");
       System.out.println("ITERATIVE TEST: " + commafyR(Integer.parseInt(s)) + "\n");
