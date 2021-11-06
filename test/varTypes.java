bc11cf915ba16beef52dc94624b7789bea8d5df5
samanthahuaa <samanthahua1@gmail.com>
2021-11-06 13:19:27 -0400

    testing from exam 2

---
+++ test/varTypes.java
@@ -0,0 +1,19 @@
+public class varTypes {
+  public static void main(String[] args) {
+    int i = 89;
+    System.out.println("int:"+i);
+
+    byte j = 89;
+    System.out.println("byte:" +j);
+
+    short k = 89;
+    System.out.println("short:"+k);
+
+    double l = 89;
+    System.out.println("double:"+l);
+
+    long m = 89;
+    System.out.println("long:"+m);
+
+  }
+}
