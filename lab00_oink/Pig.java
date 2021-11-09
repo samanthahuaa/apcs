dbf8e7327e901e97430532665e05292efe21bd46
samanthahuaa <samanthahua1@gmail.com>
2021-11-09 01:18:05 -0500

    lab

--- lab00_oink/Pig.java
+++ lab00_oink/Pig.java
@@ -1,112 +1,56 @@
-/***
- * nnnn - Samantha Hua & Courtney Huang
- * APCS
- * L00: Etterbay Odingcay Oughthray Ollaborationcay
- * 2021-11-09
- * time spent: 0.8 hrs
- * DISCO
- * 
- * 
- * QCC
- * 
- *
- * class Pig
- * a Pig Latin translator
- * ~~~~~~~~~~~~~~~~~~~ SKELETON ~~~~~~~~~~~~~~~~~~~
- *           9
- *     ,--.-'-,--.
- *     \  /-~-\  /
- *    / )' a a `( \
- *   ( (  ,---.  ) )
- *    \ `(_o_o_)' /
- *     \   `-'   /
- *      | |---| |
- *      [_]   [_]
- *      PROTIP: Make this class compilable first,
- *      then develop and test one method at a time.
- *      NEVER STRAY TOO FAR FROM COMPILABILITY/RUNNABILITY!
- ***/
+// code to be added to your growing Pig Latin translator
 
-public class Pig{
-  //Q: How does this initialization make your life easier?
-  private static final String VOWELS = "aeiou";
+public class Pig {
+
+    private static final String VOWELS = "aeiouy";
+    private static final String CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String PUNCS = ".,:;!?";
 
 
-  /*=====================================
-    boolean hasA(String,String) -- checks for a letter in a String
-    pre:  w != null, letter.length() == 1
-    post: hasA("cat", "a") -> true
-    hasA("cat", "p")       -> false
-    =====================================*/
-  public static boolean hasA( String w, String letter ){
-    return (w.indexOf(letter) != -1);
-  }//end hasA()
-
-
-  /*=====================================
-    boolean isAVowel(String) -- tells whether a letter is a vowel
-    precondition: letter.length() == 1
-    =====================================*/
-  public static boolean isAVowel( String letter ){
-    return (hasA(VOWELS, letter));
-  }
-
-
-  /*=====================================
-    int countVowels(String) -- counts vowels in a String
-    pre:  w != null
-    post: countVowels("meatball") -> 3
-    =====================================*/
-  public static int countVowels( String w ){
-    int count = 0;
-    for(int i = 0; i < w.length(); i++){
-      if(isAVowel(w.substring(i, i + 1))){
-        count ++;
-      }
+    /*=====================================
+      boolean isPunc(String) -- tells whether a character is punctuation
+      pre:  symbol.length() == 1
+      post: isPunc(".") -> true
+            isPunc("b") -> false
+      =====================================*/
+    public static boolean isPunc( String symbol ) {
+	    return PUNCS.indexOf( symbol ) != -1;
     }
-    return count;
-  }
 
 
-  /*=====================================
-    boolean hasAVowel(String) -- tells whether a String has a vowel
-    pre:  w != null
-    post: hasAVowel("cat") -> true
-    hasAVowel("zzz")       -> false
-    =====================================*/
-  public static boolean hasAVowel( String w ){
-      for (int i = 0; i < w.length(); i ++){
-        if (isAVowel(w.substring(i, i + 1))){
-          return true;
-        }
-      }
-     return false;
-  }
+    /*=====================================
+      boolean isUpperCase(String) -- tells whether a letter is uppercase
+      pre:  letter.length() == 1
+      post: isUpperCase("a") -> false
+            isUpperCase("A") -> true
+      =====================================*/
+    public static boolean isUpperCase( String letter ) {
 
-
-  /*=====================================
-    String allVowels(String) -- returns vowels in a String
-    pre:  w != null
-    post: allVowels("meatball") -> "eaa"
-    =====================================*/
-  public static String allVowels( String w ){
-    String ans = "";
-      for (int i = 0; i < w.length(); i ++){
-        if (isAVowel(w.substring(i, i + 1))){
-          ans += w.substring(i, i + 1);
-        }
-      }
-    return ans;
-  }
-
-  public static void main( String[] args ){
-    for (String s : args ) {
-      //System.out.println(hasA("cat","s"));
-      //System.out.println(isAVowel(s));
-      //System.out.println(countVowels(s));
-      //System.out.println(hasAVowel(s));
-      System.out.println(allVowels(s));
     }
-  }//end main()
+
+
+    /*=====================================
+      boolean hasPunc(String) -- tells whether a String contains punctuation
+      pre:  w != null
+      post: hasPunc("cat.") -> true
+            hasPunc("cat") -> false
+      =====================================*/
+    public static boolean hasPunc( String w ) {
+
+    }
+
+
+    /*=====================================
+      boolean beginsWithUpper(String) -- tells whether 1st letter is uppercase
+      pre:  w != null and w.length() > 0
+      post: beginsWithUpper("Apple") -> true
+            beginsWithUpper("apple") -> false
+      =====================================*/
+    public static boolean beginsWithUpper( String w ) {
+
+	return isUpperCase(w.substring(0,1) );
+    }
+
+
 
 }//end class Pig
