df43d85d83f083adfca208dbe2e0bbc644386901
samanthahuaa <samanthahua1@gmail.com>
2021-12-02 21:33:15 -0500

    testing

--- test/preIncrementOperator.java
+++ test/preIncrementOperator.java
@@ -12,17 +12,19 @@ public class preIncrementOperator {
     int i = 0;
     int x = 0;
     x = i++ +3;
-    System.out.println(x);
-    System.out.println(i);
+    System.out.println("1st assignment");
+    System.out.println(x); //3
+    System.out.println(i); //1
     x = --x + i++ + x;
-    System.out.println(x);
-    System.out.println(i);
+    System.out.println("2nd assignment");
+    System.out.println(x); //5 (2+1+2)
+    System.out.println(i); //2
 
     int j = 0;
     int k = 0;
     j = ++k +3;
-    System.out.println(j);
-    System.out.println(k);
+    System.out.println(j); //4
+    System.out.println(k); //1
   }
 
 }
