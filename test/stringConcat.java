220745b998ec501648f3573490e214815d07a5e3
Samantha Hua <shua30@stuy.edu>
2021-12-02 21:31:29 -0500

    testing

--- test/stringConcat.java
+++ test/stringConcat.java
@@ -6,5 +6,8 @@ public class stringConcat {
     String i = "l";
     String j = "o";
     System.out.println(x+y+z+i+j);
+    System.out.println("1" + new String("2") + 3 + 4);
+    System.out.println(1+2+3);
+    System.out.println(1+2+3+"4");
   }
 }
