8d24fed9844347c6295cf1d9eab25d711c7a6ec1
Samantha Hua <65002888+samanthahuaa@users.noreply.github.com>
2021-12-14 21:51:03 -0500

    Update GuessNumber.java

--- 47_guess-a-num/GuessNumber.java
+++ 47_guess-a-num/GuessNumber.java
@@ -1,13 +1,11 @@
-// Clyde "Thluffy" Sinclair
-// APCS pd0
-// HW47 -- ?
-// 2021-12-15w
-// time spent: _ hrs
+// Team Frogs (Ruby Friedman, Ivina Wang, Samantha Hua)
+// APCS pd07
+// HW47 -- Guess Again
+// 2021-12-15
+// time spent: 0.5 hrs
 
 /***
  * class GuessNumber -- fun fun fun!
- *
- * SKELETON
  *
  * eg, sample interaction with end user:
  *
@@ -21,14 +19,19 @@
 
 /***
     DISCO:
+    * If your plan is succint and accurate, the implementation of it should be quick and painless.
+    * return; allows the method to finish running in a void method without breaking.
+    * The base case is very similar for both iterative and recursive methods.
 
     QCC:
+    * How do we prevent the user from inputing out of bound inputs, (ex: range is 30-70 and user inputs 75)
+    * If while (true) keeps running until a return or break, what does while (false) do?
+    * Why is it better to have a while (true) statement as opposed to a conditional?
 
  ***/
 import java.util.Scanner;
 
-public class GuessNumber
-{
+public class GuessNumber {
 
   //instance vars
   private int _lo, _hi, _guessCtr, _target;
@@ -62,6 +65,7 @@ public class GuessNumber
     //3 cases: we either found it, too hi, too lo
     if (_target == guess) {
       System.out.println("Correct! It took "+_guessCtr+" guesses");
+      return;
     }
     else if (_target > guess) {
       System.out.println("Too low");
@@ -82,29 +86,35 @@ public class GuessNumber
     void playIter() -- Prompts a user to guess until guess is correct.
     Uses iteration.
     ==================================================*/
-  public void playIter()
-  {
+  public void playIter() {
 
     int guess;
 
     while( true ) {
       System.out.print("Guess a num bt " + _lo + " & " + _hi + ": ");
       guess = sc.nextInt();
-
       //3 cases: we either found it, too hi, too lo
-
-      /* YOUR CODE HERE */
-
+      if (_target == guess) {
+ 	System.out.println("Correct! It took "+_guessCtr+" guesses");
+      	return;
+      }
+      else if (_target > guess) {
+        System.out.println("Too low");
+        _lo = guess + 1;
+      }
+      else if (_target < guess) {
+        System.out.println("Too high");
+        _hi = guess - 1;
+      }
       _guessCtr++;
     }
   }
 
 
   //wrapper for playRec/playIter to simplify calling
-  public void play()
-  {
+  public void play() {
     //use one or the other below:
-    //playRec();
+   // playRec();
     playIter();
   }
 
@@ -112,13 +122,11 @@ public class GuessNumber
   //main method to run it all
   public static void main( String[] args )
   {
-    /*-----------------------------
     //instantiate a new game
     GuessNumber g = new GuessNumber(1,100);
 
     //start the game
     g.play();
-    -----------------------------*/
   }
 
 }//end class GuessNumber
