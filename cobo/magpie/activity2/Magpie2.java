57a683686f625d9fea94ffb157f66777556ce4f5
Samantha Hua <shua30@stuy.edu>
2022-01-12 13:19:37 -0500

    classwork

---
+++ cobo/magpie/activity2/Magpie2.java
@@ -0,0 +1,108 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ * 		    Handles responding to simple words and phrases
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie2
+{
+	/**
+	 * Get a default greeting
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
+
+	/**
+	 * Gives a response to a user statement
+	 *
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		statement = statement.trim();
+		String response = "";
+
+		if (statement.length() == 0) {
+			response = "Say something, I'm giving up on youuuuuu";
+		}
+		// if (trim(statement).)
+		else if (statement.indexOf("no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+		else if (statement.indexOf("mother") >= 0
+				|| statement.indexOf("father") >= 0
+				|| statement.indexOf("sister") >= 0
+				|| statement.indexOf("brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0)
+		{
+			response = "Tell me more about your pets";
+		}
+		else if (statement.indexOf("Mykolyk") >= 0)
+		{
+			response = "Ooo he is my teacher as well. isn't class always interesting?";
+		}
+		else if (statment.indexOf("Earth") >= 0)
+		{
+			response = "Ah yes the blue marble";
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 6;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "no lies detected";
+		}
+		else if (whichResponse == 5) {
+			response = "Excuse me?????";
+		}
+
+		return response;
+	}
+}
