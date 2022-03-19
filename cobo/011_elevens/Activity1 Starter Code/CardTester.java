f9b0e94702f628072e39d7819da7eab53ac19217
samanthahuaa <samanthahua1@gmail.com>
2022-03-19 16:49:30 -0400

    copy over ruby

---
+++ cobo/011_elevens/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,23 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card yassification = new Card("jack", "hearts", 11);
+		Card goodbye = new Card("7", "diamonds", 7);
+		Card yehaw = new Card("4", "clubs", 4);
+		Card slaying = new Card("jack", "hearts", 11);
+
+		System.out.println(goodbye.suit());
+		System.out.println(yehaw.pointValue());
+		System.out.println(goodbye);
+		System.out.println(yassification);
+		System.out.println(yassification.matches(slaying));
+		System.out.println(yassification.matches(goodbye));
+	}
+}
