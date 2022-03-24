df25e21f695e9b75c59362caac0a195ea4666372
samanthahuaa <samanthahua1@gmail.com>
2022-03-24 07:53:14 -0400

    copy

---
+++ 80_llistgen/ListTester.java
@@ -0,0 +1,90 @@
+/***
+ * class ListTester
+ * ...for putting your LList through its paces
+ *
+ * Assumes local List.java (interface),
+ *  overriding List interface from standard Java library
+ **/
+
+public class ListTester
+{
+  public static void main( String[] args )
+  {
+//different testss with charectars 
+    List wu = new LList();
+
+     System.out.println(wu);
+     wu.add('W');
+
+      System.out.println(wu);
+      wu.add('H');
+
+      System.out.println(wu);
+      wu.add('Y');
+
+      System.out.println(wu);
+
+//Test with ints
+  /*  //instantiate... var type List, obj type LList
+    List wu = new LList();
+
+    System.out.println(wu);
+    wu.add(3);
+
+    System.out.println(wu);
+    wu.add(2);
+
+    System.out.println(wu);
+    wu.add(1);
+
+    System.out.println(wu);
+
+//tests with strings
+  /*  List wu = new LList();
+
+   System.out.println(wu);
+    wu.add("RZA");
+
+    System.out.println(wu);
+    wu.add("GZA");
+
+    System.out.println(wu);
+    wu.add("ODB");
+
+    System.out.println(wu);
+    wu.add("Inspectah Deck");
+
+    System.out.println(wu);
+    wu.add("Raekwon the Chef");
+
+    System.out.println(wu);
+    wu.add("U-God");
+
+    System.out.println(wu);
+    wu.add("Ghostface");
+
+    System.out.println(wu);
+    wu.add("Method Man");
+
+    System.out.println(wu);
+
+
+  /*  for( int i=0; i<7; i++ ) {
+      int n = (int)( wu.size() * Math.random() );
+      String imposter = "@";
+      System.out.println("adding imposter at index " + n + "...");
+      wu.add( n, imposter );
+      System.out.println("Updated list: " + wu);
+    }
+
+    while( wu.size() > 0 ) {
+      int n = (int)( wu.size() * Math.random() );
+      System.out.println("deleting node "+ n + "...");
+      wu.remove(n);
+      System.out.println("Updated list: " + wu);
+    }
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+  }//end main()
+
+}//end class
