26fe28c16fbed45b5d6cd39564a93d7277b9a5b7
Samantha Hua <samanthahua1@gmail.com>
2022-04-04 21:34:04 -0400

    visible confusion

--- 88_rqueue/RQueue.java
+++ 88_rqueue/RQueue.java
@@ -25,51 +25,83 @@ public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
   // default constructor creates an empty queue
   public RQueue()
   {
-
+    _front = _end = null;
+    _size = 0;
   }
 
 
-  public void enqueue( T enQVal )
+  public void enqueue( SWASHBUCKLE enQVal )
   {
-
-  }//O(?)
+    //special case: when enqueuing to an empty list,
+    //make _front && _end point to same node
+    if ( isEmpty() ) {
+      _front = _end = new LLNode<SWASHBUCKLE>( enQVal, null );
+    }
+    else {
+      _end.setNext( new LLNode<SWASHBUCKLE>( enQVal, null ) );
+      _end = _end.getNext();
+    }
+    _size++;
+    System.out.println("enqueued " + enQVal);
+  }//O(1) because we have a pointer for the end of the queue
 
 
   // remove and return thing at front of queue
   // assume _queue ! empty
-  public T dequeue()
+  public SWASHBUCKLE dequeue()
   {
+    SWASHBUCKLE retVal = _front.getCargo();
+    _front = _front.getNext();
 
-  }//O(?)
+    if ( _front == null ) //just moved past last node
+      _end = null;      //update _end to reflect emptiness
+
+    _size--;
+
+    return retVal;
+  }//O(1)
 
 
-  public T peekFront()
+  public SWASHBUCKLE peekFront()
   {
-
-  }//O(?)
+    return _front.getCargo();
+  }//O(1)
 
 
   /***
    * void sample() -- a means of "shuffling" the queue
    * Algo:
-   *   < YOUR SUCCINCT SUMMARY HERE >
+   *   We dequeue a random number of elements and enqueue them back. this creates
+   *   a random arrangement of the items in the queue but it also preserves their order.
+   *   What that means is that we can eventually return to our original queue.
    **/
   public void sample ()
   {
-
-  }//O(?)
+    int randomInt = (int) (_size* Math.random());
+    for (int i=0; i < randomInt; i++) {
+      SWASHBUCKLE temp = dequeue();
+      enqueue(temp);
+    }
+  }//O(n) because the worst case scenario is that the entire queue must be iterated through
 
 
   public boolean isEmpty()
   {
     return _front == null;
-  } //O(?)
+  } //O(1)
 
 
   // print each node, separated by spaces
   public String toString()
   {
-
+    String retStr = "FRONT :: ";
+    LLNode<SWASHBUCKLE> tmp = _front; //init tr
+    while( tmp != null ) {
+      retStr += tmp.getCargo() + " ";
+      tmp = tmp.getNext();
+    }
+    retStr += " :: END";
+    return retStr;
   }//end toString()
 
 
@@ -78,11 +110,10 @@ public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
   public static void main( String[] args )
   {
 
-      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
-    
+
     Queue<String> PirateQueue = new RQueue<String>();
 
-    System.out.println("\nnow enqueuing..."); 
+    System.out.println("\nnow enqueuing...");
     PirateQueue.enqueue("Dread");
     PirateQueue.enqueue("Pirate");
     PirateQueue.enqueue("Roberts");
@@ -90,10 +121,10 @@ public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
     PirateQueue.enqueue("Peter");
     PirateQueue.enqueue("Stuyvesant");
 
-    System.out.println("\nnow testing toString()..."); 
+    System.out.println("\nnow testing toString()...");
     System.out.println( PirateQueue ); //for testing toString()...
 
-    System.out.println("\nnow dequeuing..."); 
+    System.out.println("\nnow dequeuing...");
     System.out.println( PirateQueue.dequeue() );
     System.out.println( PirateQueue.dequeue() );
     System.out.println( PirateQueue.dequeue() );
@@ -102,9 +133,10 @@ public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
     System.out.println( PirateQueue.dequeue() );
 
     System.out.println("\nnow dequeuing fr empty queue...\n" +
-                       "(expect NPE)\n"); 
+                       "(expect NPE)\n");
     System.out.println( PirateQueue.dequeue() );
 
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
 
   }//end main
