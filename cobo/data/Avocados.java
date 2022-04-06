f6084a81d9296d6fda7ae51147f09ef289763e2f
Samantha Hua <shua30@stuy.edu>
2022-04-06 12:43:02 -0400

    que

--- cobo/data/Avocados.java
+++ cobo/data/Avocados.java
@@ -4,9 +4,20 @@ import java.util.Scanner;
 
 public class Avocados {
    public static void main(String[] args) {
-      DataSource ds = DataSource.connect("https://www.kaggle.com/datasets/neuromusic/avocado-prices>
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/s>
-             "station/station_id", "station/state",
-             "station/latitude", "station/longitude");
+      DataSource ds = DataSource.connect("https://www.kaggle.com/datasets/neuromusic/avocado-prices").load();
+      ArrayList<AvocadoPrices> allstns = ds.fetchList("AvocadoPrices", "avocado/date"
+             "avocado/averagePrice", "avocado/type",
+             "avocado/year", "avocado/region", "avocado/volume");
+      System.out.println("Total avocado datapoints: " + allstns.size());
+
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a state abbreviation: ");
+      String state = sc.next();
+      System.out.println("Stations in " + state);
+      for (WeatherStation ws : allstns) {
+         if (ws.isLocatedInState(state)) {
+            System.out.println("  " + ws.getId() + ": " + ws.getName());
+         }
+      }
 	}
 }
